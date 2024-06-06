b789d3c9ee904249f33f0642cbe2ce41da04aa71
csirick2020 <144964085+csirick2020@users.noreply.github.com>
2024-06-06 15:05:31 -0500

    Update sp500_analyst_module.py
    Code refactoring...got rid of redundant variable.

--- sp500_analyst_module.py
+++ sp500_analyst_module.py
@@ -7,17 +7,19 @@ from sp500_ticker_retrieval import load_sp500_tickers
 ticker_list = load_sp500_tickers()
 
 # Replace '.' with '-' in each ticker symbol
-ticker_list_hyphenated = [symbol.replace('.', '-') for symbol in ticker_list.split()]
+ticker_list_hyphenated = [symbol.upper().replace('.', '-') for symbol in ticker_list.split()]
 
 # Create a Tickers object with all ticker symbols
 sp500_data = yf.Tickers(' '.join(ticker_list_hyphenated))
 
-# Extract ticker symbols from the Tickers object
-tick_symbols = list(sp500_data.tickers.keys())
+# Get the current date and calculate the date for the previous day
+current_date = datetime.date.today()
+previous_date = current_date - datetime.timedelta(days=1)
 
+# Prompt user for input
 day_to_check = input("Would you like to check today's analyst activity or yesterday's?\n\
 Enter 1 for today or 2 for yesterday: ")
-
+print()
 
 if day_to_check == "1":
     # For today...
@@ -26,13 +28,12 @@ elif day_to_check == "2":
     # For the previous day...
     print("Retrieving yesterday's analyst data for S&P500 stocks:")
 print()
+
 # Iterate through each ticker symbol and access its data
-for symbol in tick_symbols:
+for symbol in ticker_list_hyphenated:
     ticky = sp500_data.tickers[symbol]
     actions = ticky.upgrades_downgrades
-    # Get the current date and calculate the date for the previous day
-    current_date = datetime.date.today()
-    previous_date = current_date - datetime.timedelta(days=1)
+
     # Filter the DataFrame and display entries
     if day_to_check == "1":
         current_day_actions = actions[actions.index.date == current_date]
@@ -43,9 +44,10 @@ for symbol in tick_symbols:
                                                    (current_day_actions['ToGrade'].isin(['Buy', 'Outperform', 'Overweight']))]
             if not filtered_actions.empty:
                 # Print the filtered DataFrame
-                print(f"Analyst action(s) for {symbol.upper()} on {current_date}:")
+                print(f"Analyst action(s) for {symbol} on {current_date}:")
                 print(filtered_actions)
                 print()
+
     elif day_to_check == "2":
         # Filter the DataFrame and display entries
         previous_day_actions = actions[actions.index.date == previous_date]
@@ -56,6 +58,6 @@ for symbol in tick_symbols:
                                                    (previous_day_actions['ToGrade'].isin(['Buy', 'Outperform', 'Overweight']))]
             if not filtered_actions.empty:
                 # Print the filtered DataFrame
-                print(f"Analyst action(s) for {symbol.upper()} on {previous_date}:")
+                print(f"Analyst action(s) for {symbol} on {previous_date}:")
                 print(filtered_actions)
                 print()
